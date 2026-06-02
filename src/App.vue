9ea2b900fdb60239fcb7203af573660637cb7a20
Kevin Choque Apaza <kevinchoque-sif@itpm.edu.bo>
2026-06-02 19:19:25 -0400

    SEGUNDO CAMBIO

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ const listaProductos: Producto[] = [
 <div class="container mt-5 text-center" style="font-family: sans-serif;"> 
 <div class="alert alert-success shadow"> 
 <h1> Panel de Almacén - ITPM</h1> 
-<p>Asignatura: DPW-207 | Lic. Félix Maldonado</p> 
+<p>Asignatura: DPW-207 | KEVIN CHOQUE APAZA</p> 
 <hr> 
 <ul style="list-style: none; padding: 0;">
    <li v-for="p in listaProductos" :key="p.id" style="margin-bottom: 10px;"> 
